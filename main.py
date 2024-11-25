b7d144528ab13f3b6f0363dd2b9c4d0baf0faa19
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-11-25 13:22:57 +0300

    password save fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from hashlib import sha256
 from flask import Flask
 import datetime
 import json
@@ -5,14 +6,23 @@ import os
 
 app = Flask(__name__)
 
+def hash(v):
+    return sha256(v.encode()).hexdigest()
+
+def check_username(user):
+    return os.path.exists(f"accounts/{user}")
+
+def check_password(user, passwd):
+    if not check_username(user):
+        return False
+    passwords = json.loads(open("passwords.json").read())
+    return passwords[user] == hash(passwd)
+
 @app.route('/send/<content>/<sender>/<passwd>/<user>')
 def send(content, sender, passwd, user):
     passwords = json.loads(open("passwords.json").read())
-    if not os.path.exists(f"accounts/{sender}"):
+    if not check_password(sender, passwd):
         print("failed: username incorrect")
-        return f"Failed: login/password incorrect"
-    if passwords[sender] != passwd:
-        print("failed: password incorrect")
         return f"Failed: login/password incorrect"
     dt = f"{datetime.datetime.now()}".replace(' ', '-')
     with open(f"accounts/{sender}/sended.txt", 'a') as f:
@@ -28,32 +38,44 @@ def send(content, sender, passwd, user):
 @app.route('/get_all_rmessages/<passwd>/<user>')
 def get_all_rmessages(passwd, user):
     passwords = json.loads(open("passwords.json").read())
-    if not os.path.exists(f"accounts/{user}"):
+    if not check_password(user, passwd):
         print("failed: username incorrect")
         return f"Failed: login/password incorrect"
-    if passwords[user] != passwd:
-        print("failed: password incorrect")
-        return f"Failed: login/password incorrect"
     return open(f"accounts/{user}/recived.txt").read()
+
+@app.route('/clear_all_rmessages/<passwd>/<user>')
+def clear_all_rmessages(passwd, user):
+    passwords = json.loads(open("passwords.json").read())
+    if not check_password(user, passwd):
+        print("failed: username incorrect")
+        return f"Failed: login/password incorrect"
+    open(f"accounts/{user}/recived.txt", 'w').write("")
+    return "Cleared"
+
+@app.route('/clear_all_smessages/<passwd>/<user>')
+def clear_all_smessages(passwd, user):
+    passwords = json.loads(open("passwords.json").read())
+    if not check_password(user, passwd):
+        print("failed: username incorrect")
+        return f"Failed: login/password incorrect"
+    open(f"accounts/{user}/sended.txt", 'w').write("")
+    return "Cleared"
 
 @app.route('/get_all_smessages/<passwd>/<user>')
 def get_all_smessages(passwd, user):
     passwords = json.loads(open("passwords.json").read())
-    if not os.path.exists(f"accounts/{user}"):
+    if not check_password(user, passwd):
         print("failed: username incorrect")
-        return f"Failed: login/password incorrect"
-    if passwords[user] != passwd:
-        print("failed: password incorrect")
         return f"Failed: login/password incorrect"
     return open(f"accounts/{user}/sended.txt").read()
 
 @app.route('/reg/<passwd>/<user>')
 def reg(passwd, user):
-    if os.path.exists(f"accounts/{user}"):
+    if check_username(user):
         print("failed: username incorrect")
         return f"Failed: user exists"
     passwords = json.loads(open("passwords.json").read())
-    passwords[user] = passwd
+    passwords[user] = hash(passwd)
     json.dump(passwords, open("passwords.json", 'w'))
     os.mkdir(f"accounts/{user}")
     return "Registered"
