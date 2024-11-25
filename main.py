0adc5763045e0400bf42f811230ab0c8fc56502e
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-11-25 15:06:02 +0300

    security patch

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
-from hashlib import sha256
+from hashlib import sha256, sha512
+import atexit
 from flask import Flask
 import datetime
 import json
+import stat
 import os
 
+def get_chmod(path):
+    return stat.S_IMODE(os.stat(path).st_mode)
+
+def srm(path):
+    for _ in range(10):
+        with open(path, 'wb') as f:
+            f.write(os.urandom(5000))
+    os.remove(demail_keys_path)
+
 app = Flask(__name__)
+salt_path = "/root/.demail-salt"
+if get_chmod(salt_path) != 0o600:
+    print("ERROR: too open salt file, requires 600")
+    r = input("remove it for secure?(Y/N)")
+    if r == "Y":
+        srm(salt_path)
+    exit(1)
+salt = open(salt_path).read()
+keys = {}
+
+demail_keys_path = "/root/.demail-keys.json"
+
+if os.path.exists(demail_keys_path):
+    if get_chmod(salt_path) != 0o600:
+        print("ERROR: too open keys file, requires 600")
+        r = input("remove it for secure?(Y/N)")
+        if r == "Y":
+            srm(demail_keys_path)
+        exit(1)
+    with open(demail_keys_path, 'r') as f:
+        keys = json.load(f)
+
+    srm(demail_keys_path)
+
+@atexit.register
+def cleanup():
+    print("saving keys...")
+    with open(demail_keys_path, 'w') as f:
+        json.dump(keys, f)
+    os.chmod(demail_keys_path, 0o600)
 
 def hash(v):
     return sha256(v.encode()).hexdigest()
@@ -12,19 +53,40 @@ def hash(v):
 def check_username(user):
     return os.path.exists(f"accounts/{user}")
 
+def get_password(user):
+    passwords = json.loads(open("passwords.json").read())
+    return passwords[user]
+
 def check_password(user, passwd):
     if not check_username(user):
         return False
-    passwords = json.loads(open("passwords.json").read())
-    return passwords[user] == hash(passwd)
+    return get_password(user) == hash(passwd)
+
+def generate_key(p, s, u):
+    global keys
+    key = hash(p + s + salt + os.urandom(32).hex())
+    keys[s] = key
+    return key
+
+def simple_encoder(raw_key, message):
+    key = int.from_bytes(sha512(raw_key.encode()).digest(), 'big')
+    for _ in range(32):
+        key ^= key << 8
+        key ^= key >> 16
+    msg = int.from_bytes(message, 'big') ^ key
+
+    return msg.to_bytes((msg.bit_length() + 7) // 8, 'big')
+
+print(simple_encoder("123", simple_encoder("122", b"hello, world!")))
 
 @app.route('/send/<content>/<sender>/<passwd>/<user>')
 def send(content, sender, passwd, user):
-    passwords = json.loads(open("passwords.json").read())
     if not check_password(sender, passwd):
         print("failed: username incorrect")
         return f"Failed: login/password incorrect"
     dt = f"{datetime.datetime.now()}".replace(' ', '-')
+    key = generate_key(passwd, sender, user)
+    content = simple_encoder(key, content.encode()).hex()
     with open(f"accounts/{sender}/sended.txt", 'a') as f:
         f.write(f"{user} {dt} {content}\n")
 
@@ -37,15 +99,21 @@ def send(content, sender, passwd, user):
 
 @app.route('/get_all_rmessages/<passwd>/<user>')
 def get_all_rmessages(passwd, user):
-    passwords = json.loads(open("passwords.json").read())
     if not check_password(user, passwd):
         print("failed: username incorrect")
         return f"Failed: login/password incorrect"
-    return open(f"accounts/{user}/recived.txt").read()
+    msgs = open(f"accounts/{user}/recived.txt").read().split('\n')
+    out = ""
+    for msg in msgs:
+        sp = msg.split(maxsplit=3)
+        if len(sp) != 3:
+            continue
+        c = bytes.fromhex(sp[2])
+        out += f'{sp[0]}({sp[1]}): {simple_encoder(keys[sp[0]], c).decode()}'
+    return out
 
 @app.route('/clear_all_rmessages/<passwd>/<user>')
 def clear_all_rmessages(passwd, user):
-    passwords = json.loads(open("passwords.json").read())
     if not check_password(user, passwd):
         print("failed: username incorrect")
         return f"Failed: login/password incorrect"
@@ -54,7 +122,6 @@ def clear_all_rmessages(passwd, user):
 
 @app.route('/clear_all_smessages/<passwd>/<user>')
 def clear_all_smessages(passwd, user):
-    passwords = json.loads(open("passwords.json").read())
     if not check_password(user, passwd):
         print("failed: username incorrect")
         return f"Failed: login/password incorrect"
@@ -63,11 +130,19 @@ def clear_all_smessages(passwd, user):
 
 @app.route('/get_all_smessages/<passwd>/<user>')
 def get_all_smessages(passwd, user):
-    passwords = json.loads(open("passwords.json").read())
     if not check_password(user, passwd):
         print("failed: username incorrect")
         return f"Failed: login/password incorrect"
-    return open(f"accounts/{user}/sended.txt").read()
+    msgs = open(f"accounts/{user}/sended.txt").read().split('\n')
+    out = ""
+    key = keys[user]
+    for msg in msgs:
+        sp = msg.split(maxsplit=3)
+        if len(sp) != 3:
+            continue
+        c = bytes.fromhex(sp[2])
+        out += f'{simple_encoder(key, c).decode()}({sp[1]}) -> {sp[0]}'
+    return out
 
 @app.route('/reg/<passwd>/<user>')
 def reg(passwd, user):
