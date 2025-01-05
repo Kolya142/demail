9e1b21742c94634c07ad92b31744bf02f5797bd8
Kolya142 <87275865+Kolya142@users.noreply.github.com>
2025-01-05 19:11:51 +0300

    change to 9999

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn 'main:create_app()' -b 0.0.0.0:9932 --certfile=../cert.crt --keyfile=../cert.key -w 1
+gunicorn 'main:create_app()' -b 0.0.0.0:9999 --certfile=../cert.crt --keyfile=../cert.key -w 1
